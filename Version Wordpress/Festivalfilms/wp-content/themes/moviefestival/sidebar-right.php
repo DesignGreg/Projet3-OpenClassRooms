285c80aee31270b0ff9e113f56100b3ba7cda80c
Gregory <huyghegregory@yahoo.fr>
2018-10-01 09:21:43 +0200

    custom querie pour afficher les derniers articles sur la sidebar-right plutôt que archives

--- Version Wordpress/Festivalfilms/wp-content/themes/moviefestival/sidebar-right.php
+++ Version Wordpress/Festivalfilms/wp-content/themes/moviefestival/sidebar-right.php
@@ -1,13 +1,28 @@
-<!-- Plugin affichant les derniers articles publiés -->
 <aside class="main-content__list col-lg-3 col-md-12 animated fadeInRight">
-    <div class="main-content__list--padding">
+  <div class="main-content__list--padding">
+   <?php 
+                
+        $homepagePosts = new WP_Query(array(
+            'posts_per_page' => 3
+        ));
+
+        while($homepagePosts -> have_posts()) {
+            $homepagePosts -> the_post(); ?>
+   
+    
         <div class="main-content__list--article">
             <h3 class="main-content__list--title">
-                <?php _e('Actu du festival'); ?></h3>
+                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+            </h3>
                 <ul class="main-content__list--text">
-                    <?php wp_get_archives('type=postbypost&limit=3'); ?>
+                    <?php echo wp_trim_words(get_the_content(), 18); ?> <a href="<?php the_permalink(); ?>">Continuez</a>
                 </ul>
-        </div>
+        
 
     </div>
+    
+    <?php } wp_reset_postdata();
+    
+    ?>
+   </div> 
 </aside>
